9606c2f91c9146e7627318316e18a6936e4c3176
Kaushik Chauhan <kaushik.chauhan23@gmail.com>
2025-01-03 06:10:31 +0000

    Sample if else programs

---
+++ Conditionals_and_Loops/tut2/tut2.c
@@ -0,0 +1,15 @@
+/*Program to understand If else estatements */
+#include <stdio.h>
+
+int main()
+{
+    if (sizeof(void))
+    {
+        printf("kaushik\n");
+        printf("Size of void is %ld\n\n", sizeof(void));
+    }
+    else
+    {
+        printf("chauhan");
+    }
+}
