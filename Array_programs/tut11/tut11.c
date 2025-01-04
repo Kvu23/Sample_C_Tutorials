78693c74c069868915d22fea210801ef6754e805
Kaushik Chauhan <kaushik.chauhan23@gmail.com>
2025-01-04 17:18:16 +0000

    Sample C program exercises to do matrix substraction

---
+++ Array_programs/tut11/tut11.c
@@ -0,0 +1,45 @@
+/* substraction of matrix using 2d array */
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j, a[3][3], b[3][3], c[3][3];
+
+    printf("please enter elements of matrix a:  \n");
+    for (i = 0; i < 3; ++i)
+    {
+        for (j = 0; j < 3; ++j)
+        {
+            scanf("%d", &a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("please enter elements of matrix b:  \n");
+    for (i = 0; i < 3; ++i)
+    {
+        for (j = 0; j < 3; ++j)
+        {
+            scanf("%d", &b[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < 3; ++i)
+    {
+        for (j = 0; j < 3; ++j)
+        {
+            c[i][j] = a[i][j] - b[i][j];
+        }
+    }
+    printf("\n resultant matrix c is :   \n");
+    for (i = 0; i < 3; ++i)
+    {
+        for (j = 0; j < 3; ++j)
+        {
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
