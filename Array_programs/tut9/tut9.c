6fe4e3ddd67408ad337a9050532a7894fe55cf24
Kaushik Chauhan <kaushik.chauhan23@gmail.com>
2025-01-04 17:03:40 +0000

    Sample C program exercises to sort array elemnts in descending order

---
+++ Array_programs/tut9/tut9.c
@@ -0,0 +1,33 @@
+/* to sort the array of elements descending*/
+
+#include <stdio.h>
+int main()
+{
+    int arr[10], i, j, num, temp;
+    printf("please enter size of an array:   ");
+    scanf("%d", &num);
+    printf("please enter elements of an array:  ");
+
+    for (i = 0; i < num; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 0; i < num - 1; ++i)
+    {
+        for (j = i + 1; j < num; ++j)
+        {
+            if (arr[i] < arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    printf("\n sorted list of array in descending order \n");
+    for (i = 0; i < num; ++i)
+    {
+        printf("%d\n", arr[i]);
+    }
+    return 0;
+}
