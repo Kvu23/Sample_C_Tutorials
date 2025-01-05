4ce39e9be935f0b52bf3c4547f1bb322d9e8f97d
Kaushik Chauhan <kaushik.chauhan23@gmail.com>
2025-01-05 04:19:36 +0000

    Sample C program exercises to check factorial using iterative and recursive way

--- tut35/tut35.c
+++ tut35/tut35.c
@@ -1,14 +1,25 @@
-/* factorial using recursive function */
+/* factorial using recursive function and via loop*/
 #include <stdio.h>
 
-int factorial(unsigned long int i)
+long factorial_recursive(unsigned long int i)
 {
 
     if (i <= 1)
     {
         return 1;
     }
-    return i * factorial(i - 1);
+    return i * factorial_recursive(i - 1);
+}
+
+long factorial_iterative(int n)
+{
+    int c;
+    long result = 1;
+
+    for (c = 1; c <= n; c++)
+        result = result * c;
+
+    return result;
 }
 
 int main()
@@ -16,6 +27,7 @@ int main()
     unsigned long int i;
     printf("Enter a number to find factorial : ");
     scanf("%lu", &i);
-    printf("Factorial of %ld is %u\n", i, factorial(i));
+    printf("Factorial of %ld is %lu\n", i, factorial_recursive(i));
+    printf("Factorial of %ld is %lu\n", i, factorial_iterative(i));
     return 0;
 }
