cc730a9ab61944d106f5a1522cb93cc26c5350c0
Kaushik Chauhan <kaushik.chauhan23@gmail.com>
2025-01-05 04:26:15 +0000

    Sample C program exercises to check palindrome using iterative and recursive way

--- tut23/tut23.c
+++ tut23/tut23.c
@@ -1,6 +1,20 @@
 /* C program to check whether a number is palindrome or not */
 
 #include <stdio.h>
+
+int CheckPalindrome(int num)
+{
+    static int sum = 0, r;
+
+    if (num != 0)
+    {
+        r = num % 10;
+        sum = sum * 10 + r;
+        CheckPalindrome(num / 10);
+    }
+    return sum;
+}
+
 int main()
 {
     int n, reverse = 0, rem, temp;
@@ -18,5 +32,11 @@ int main()
         printf("%d is a palindrome.\n", n);
     else
         printf("%d is not a palindrome.\n", n);
+
+    if (n == CheckPalindrome(n))
+        printf("%d is a palindrome.\n", n);
+    else
+        printf("%d is not a palindrome.\n", n);
+
     return 0;
 }
