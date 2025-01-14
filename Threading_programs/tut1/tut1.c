3a035fd6d0510c4db28021a5b51cb4242e654796
Kaushik Chauhan <kaushik.chauhan23@gmail.com>
2025-01-14 05:35:50 +0000

    Sample Threading programs to create threads

---
+++ Threading_programs/tut1/tut1.c
@@ -0,0 +1,33 @@
+#include <pthread.h>
+#include <stdio.h>
+
+void *Thread1(void *arg)
+{
+    printf("Hello from thread 1\n\n");
+    return NULL;
+}
+
+void *Thread2(void *arg)
+{
+    printf("Hello from thread 2\n\n");
+    return NULL;
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    pthread_t tid1, tid2;
+
+    /*create threads*/
+    pthread_create(&tid1, NULL, Thread1, NULL);
+    pthread_create(&tid2, NULL, Thread2, NULL);
+
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+
+    printf("Hello from main thread\n");
+
+    printf("Both threads are terminated \n\n");
+
+    return 0;
+}
