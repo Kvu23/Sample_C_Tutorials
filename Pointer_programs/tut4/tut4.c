713d1d1aaf4e09104e4ba00e758fcf8d0e59f86d
KvU23 <kaushik.chauhan23@gmail.com>
2026-07-05 14:11:58 +0530

    Added sample pointer exercises

---
+++ Pointer_programs/tut4/tut4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define MERGE(a, b) (a##b)
+
+int main(void) {
+  int arr[5] = {100, 200, 300, 400, 500};
+
+  int *p = arr; // p points to the first element of arr
+  ++*p;         // Increment the value pointed to by p (arr[0] becomes 101)
+
+  printf("arr[0] = %d, arr[1] = %d, *p = %d\n", arr[0], arr[1],
+         *p); // Output: arr[0] = 101, arr[1] = 200, *p  = 101
+
+  return 0;
+}
